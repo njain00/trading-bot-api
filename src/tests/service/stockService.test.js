154a39f7a4f4a92d57ad3effa751889a6cb271c7
Harsh Jain <jainh9999@gmail.com>
2020-06-25 20:16:58 -0400

    unit testing for service post method

--- src/tests/service/stockService.test.js
+++ src/tests/service/stockService.test.js
@@ -1,199 +1,221 @@
 import StockService from '../../service/stockService.js'
 
-test('should return empty array when repo call returns empty array', async () => {
-    // Arrange
-    var stockRepository = {
-        getAllStockUsers: () => {
-            return [];
+describe('getAllStockUsers', () => {
+    test('should return empty array when repo call returns empty array', async () => {
+        // Arrange
+        var stockRepository = {
+            getAllStockUsers: () => {
+                return [];
+            }
         }
-    }
-    var logger = null;
-    var expectedStockWithUsers = [];
+        var logger = null;
+        var expectedStockWithUsers = [];
 
-    var stockService = new StockService({ logger, stockRepository });
+        var stockService = new StockService({ logger, stockRepository });
 
-    // Act
-    var actualStockWithUsers = await stockService.getAllStockUsers();
+        // Act
+        var actualStockWithUsers = await stockService.getAllStockUsers();
 
-    // Assert
-    expect(actualStockWithUsers).toStrictEqual(expectedStockWithUsers);
-});
+        // Assert
+        expect(actualStockWithUsers).toStrictEqual(expectedStockWithUsers);
+    });
 
-test('should return one stock object with one user array when repo call returns only one stock with one user record', async () => {
-    // Arrange
-    var stockRepository = {
-        getAllStockUsers: () => {
-            return [{
-                Ticker: 'MSFT',
-                CompanyName: 'Microsoft Corporation',
-                FirstName: 'Test',
-                LastName: 'McTest',
-                EmailAddress: 'test@tester.com'
-            }];
+    test('should return one stock object with one user array when repo call returns only one stock with one user record', async () => {
+        // Arrange
+        var stockRepository = {
+            getAllStockUsers: () => {
+                return [{
+                    Ticker: 'MSFT',
+                    CompanyName: 'Microsoft Corporation',
+                    FirstName: 'Test',
+                    LastName: 'McTest',
+                    EmailAddress: 'test@tester.com'
+                }];
+            }
         }
-    }
-    var logger = null;
-    var expectedStockWithUsers = [{
-        ticker: 'MSFT',
-        companyName: 'Microsoft Corporation',
-        users: [{
-                firstName: 'Test',
-                lastName: 'McTest',
-                emailAddress: 'test@tester.com'
-            }]
-    }];
-
-    var stockService = new StockService({ logger, stockRepository });
-
-    // Act
-    var actualStockWithUsers = await stockService.getAllStockUsers();
-
-    // Assert
-    expect(actualStockWithUsers).toStrictEqual(expectedStockWithUsers);
-});
-
-test('should return one stock object with array of two users when repo call returns two records of same stock', async () => {
-    // Arrange
-    var stockRepository = {
-        getAllStockUsers: () => {
-            return [{
-                Ticker: 'MSFT',
-                CompanyName: 'Microsoft Corporation',
-                FirstName: 'Test',
-                LastName: 'McTest',
-                EmailAddress: 'test@tester.com'
-            },
-            {
-                Ticker: 'MSFT',
-                CompanyName: 'Microsoft Corporation',
-                FirstName: 'Nishant',
-                LastName: 'Jain',
-                EmailAddress: 'nishant@jain.com'
-            }];
-        }
-    }
-    var logger = null;
-    var expectedStockWithUsers = [{
-        ticker: 'MSFT',
-        companyName: 'Microsoft Corporation',
-        users: [{
+        var logger = null;
+        var expectedStockWithUsers = [{
+            ticker: 'MSFT',
+            companyName: 'Microsoft Corporation',
+            users: [{
                     firstName: 'Test',
                     lastName: 'McTest',
                     emailAddress: 'test@tester.com'
+                }]
+        }];
+
+        var stockService = new StockService({ logger, stockRepository });
+
+        // Act
+        var actualStockWithUsers = await stockService.getAllStockUsers();
+
+        // Assert
+        expect(actualStockWithUsers).toStrictEqual(expectedStockWithUsers);
+    });
+
+    test('should return one stock object with array of two users when repo call returns two records of same stock', async () => {
+        // Arrange
+        var stockRepository = {
+            getAllStockUsers: () => {
+                return [{
+                    Ticker: 'MSFT',
+                    CompanyName: 'Microsoft Corporation',
+                    FirstName: 'Test',
+                    LastName: 'McTest',
+                    EmailAddress: 'test@tester.com'
                 },
                 {
-                    firstName: 'Nishant',
-                    lastName: 'Jain',
-                    emailAddress: 'nishant@jain.com'
-                }
-        ]
-    }];
+                    Ticker: 'MSFT',
+                    CompanyName: 'Microsoft Corporation',
+                    FirstName: 'Nishant',
+                    LastName: 'Jain',
+                    EmailAddress: 'nishant@jain.com'
+                }];
+            }
+        }
+        var logger = null;
+        var expectedStockWithUsers = [{
+            ticker: 'MSFT',
+            companyName: 'Microsoft Corporation',
+            users: [{
+                        firstName: 'Test',
+                        lastName: 'McTest',
+                        emailAddress: 'test@tester.com'
+                    },
+                    {
+                        firstName: 'Nishant',
+                        lastName: 'Jain',
+                        emailAddress: 'nishant@jain.com'
+                    }
+            ]
+        }];
 
-    var stockService = new StockService({ logger, stockRepository });
+        var stockService = new StockService({ logger, stockRepository });
 
-    // Act
-    var actualStockWithUsers = await stockService.getAllStockUsers();
+        // Act
+        var actualStockWithUsers = await stockService.getAllStockUsers();
 
-    // Assert
-    expect(actualStockWithUsers).toStrictEqual(expectedStockWithUsers);
+        // Assert
+        expect(actualStockWithUsers).toStrictEqual(expectedStockWithUsers);
+    });
+
+    test('should return array of two stocks with one user each when repo call returns two different stocks', async () => {
+        // Arrange
+        var stockRepository = {
+            getAllStockUsers: () => {
+                return [{
+                    Ticker: 'MSFT',
+                    CompanyName: 'Microsoft Corporation',
+                    FirstName: 'Test',
+                    LastName: 'McTest',
+                    EmailAddress: 'test@tester.com'
+                },
+                {
+                    Ticker: 'AMZN',
+                    CompanyName: 'Amazon',
+                    FirstName: 'Nishant',
+                    LastName: 'Jain',
+                    EmailAddress: 'nishant@jain.com'
+                }];
+            }
+        }
+        var logger = null;
+        var expectedStockWithUsers = [{
+            ticker: 'MSFT',
+            companyName: 'Microsoft Corporation',
+            users: [{
+                        firstName: 'Test',
+                        lastName: 'McTest',
+                        emailAddress: 'test@tester.com'
+                    }]
+        },
+        {
+            ticker: 'AMZN',
+            companyName: 'Amazon',
+            users: [{
+                        firstName: 'Nishant',
+                        lastName: 'Jain',
+                        emailAddress: 'nishant@jain.com'
+                    }]
+        }];
+
+        var stockService = new StockService({ logger, stockRepository });
+
+        // Act
+        var actualStockWithUsers = await stockService.getAllStockUsers();
+
+        // Assert
+        expect(actualStockWithUsers).toStrictEqual(expectedStockWithUsers);
+    });
+
+    test('should return array of two stocks with one user each when repo call returns two different stocks with same user', async () => {
+        // Arrange
+        var stockRepository = {
+            getAllStockUsers: () => {
+                return [{
+                    Ticker: 'MSFT',
+                    CompanyName: 'Microsoft Corporation',
+                    FirstName: 'Test',
+                    LastName: 'McTest',
+                    EmailAddress: 'test@tester.com'
+                },
+                {
+                    Ticker: 'AMZN',
+                    CompanyName: 'Amazon',
+                    FirstName: 'Test',
+                    LastName: 'McTest',
+                    EmailAddress: 'test@tester.com'
+                }];
+            }
+        }
+        var logger = null;
+        var expectedStockWithUsers = [{
+            ticker: 'MSFT',
+            companyName: 'Microsoft Corporation',
+            users: [{
+                        firstName: 'Test',
+                        lastName: 'McTest',
+                        emailAddress: 'test@tester.com'
+                    }]
+        },
+        {
+            ticker: 'AMZN',
+            companyName: 'Amazon',
+            users: [{
+                        firstName: 'Test',
+                        lastName: 'McTest',
+                        emailAddress: 'test@tester.com'
+                    }]
+        }];
+
+        var stockService = new StockService({ logger, stockRepository });
+
+        // Act
+        var actualStockWithUsers = await stockService.getAllStockUsers();
+
+        // Assert
+        expect(actualStockWithUsers).toStrictEqual(expectedStockWithUsers);
+    });
 });
 
-test('should return array of two stocks with one user each when repo call returns two different stocks', async () => {
-    // Arrange
-    var stockRepository = {
-        getAllStockUsers: () => {
-            return [{
-                Ticker: 'MSFT',
-                CompanyName: 'Microsoft Corporation',
-                FirstName: 'Test',
-                LastName: 'McTest',
-                EmailAddress: 'test@tester.com'
+describe('postCandlestickData', () => {
+    test('should post candlestick data on a ticker that already exists in the database', async () => {
+        // Arrange
+        var stockRepository = {
+            getStockTicker: () => {
+                return 'MSFT';
             },
-            {
-                Ticker: 'AMZN',
-                CompanyName: 'Amazon',
-                FirstName: 'Nishant',
-                LastName: 'Jain',
-                EmailAddress: 'nishant@jain.com'
-            }];
+            postStockMetadata: () => {},
+            postCandlestickData: () => {}
         }
-    }
-    var logger = null;
-    var expectedStockWithUsers = [{
-        ticker: 'MSFT',
-        companyName: 'Microsoft Corporation',
-        users: [{
-                    firstName: 'Test',
-                    lastName: 'McTest',
-                    emailAddress: 'test@tester.com'
-                }]
-    },
-    {
-        ticker: 'AMZN',
-        companyName: 'Amazon',
-        users: [
-                {
-                    firstName: 'Nishant',
-                    lastName: 'Jain',
-                    emailAddress: 'nishant@jain.com'
-                }]
-    }];
+        var logger = null;
+        var stockService = new StockService({ logger, stockRepository });
+        var stockData = { ticker:'MSFT' }
 
-    var stockService = new StockService({ logger, stockRepository });
+        // Act
+        await stockService.postCandlestickData(stockData);
 
-    // Act
-    var actualStockWithUsers = await stockService.getAllStockUsers();
-
-    // Assert
-    expect(actualStockWithUsers).toStrictEqual(expectedStockWithUsers);
-});
-
-test('should return array of two stocks with one user each when repo call returns two different stocks with same user', async () => {
-    // Arrange
-    var stockRepository = {
-        getAllStockUsers: () => {
-            return [{
-                Ticker: 'MSFT',
-                CompanyName: 'Microsoft Corporation',
-                FirstName: 'Test',
-                LastName: 'McTest',
-                EmailAddress: 'test@tester.com'
-            },
-            {
-                Ticker: 'AMZN',
-                CompanyName: 'Amazon',
-                FirstName: 'Test',
-                LastName: 'McTest',
-                EmailAddress: 'test@tester.com'
-            }];
-        }
-    }
-    var logger = null;
-    var expectedStockWithUsers = [{
-        ticker: 'MSFT',
-        companyName: 'Microsoft Corporation',
-        users: [{
-                    firstName: 'Test',
-                    lastName: 'McTest',
-                    emailAddress: 'test@tester.com'
-                }]
-    },
-    {
-        ticker: 'AMZN',
-        companyName: 'Amazon',
-        users: [
-                {
-                    firstName: 'Test',
-                    lastName: 'McTest',
-                    emailAddress: 'test@tester.com'
-                }]
-    }];
-
-    var stockService = new StockService({ logger, stockRepository });
-
-    // Act
-    var actualStockWithUsers = await stockService.getAllStockUsers();
-
-    // Assert
-    expect(actualStockWithUsers).toStrictEqual(expectedStockWithUsers);
+        // Assert
+        // TODO: Mock functions and measure how many times they have been as the verification
+    });
 });
